65c33424e81a5d2f21d4bc4bb68c8842331caae7
elliot <elliot.huang@vantiva.com>
2024-09-03 12:49:46 +0800

    fix

--- build.zig
+++ build.zig
@@ -175,25 +175,6 @@ pub fn build(b: *std.Build) void {
     exe.addIncludePath(b.path("."));
     exe.addIncludePath(b.path("glob"));
     b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
 
 const make_src = [_][]const u8{
@@ -225,8 +206,4 @@ const make_src = [_][]const u8{
     "vpath.c",
     "hash.c",
     "remote-stub.c",
-
-    // musl actually provides both of them
-    //"glob/glob.c",
-    //"glob/fnmatch.c",
 };
